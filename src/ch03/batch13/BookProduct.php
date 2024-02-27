ab6ea51e5362e11e75c650f6768fef9144d0de55
= <=>
2024-02-26 17:55:22 -0700

    Chapter 3, batch 13 complete

---
+++ src/ch03/batch13/BookProduct.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace popp\ch03\batch13;
+
+use popp\ch03\batch13\ShopProduct;
+
+/* listing 03.62 */
+class BookProduct extends ShopProduct
+{
+    public $numPages;
+
+    public function __construct(
+        string $title,
+        string $firstName,
+        string $mainName,
+        float $price,
+        int $numPages
+    ) {
+        parent::__construct(
+            $title,
+            $firstName,
+            $mainName,
+            $price
+        );
+
+        $this->numPages = $numPages;
+    }
+
+    public function getNumberOfPages(): int
+    {
+        return $this->numPages;
+    }
+
+    public function getSummaryLine(): string
+    {
+        $base = parent::getSummaryLine();
+        $base .= ": page count - {$this->numPages}";
+
+        return $base;
+    }
+}
