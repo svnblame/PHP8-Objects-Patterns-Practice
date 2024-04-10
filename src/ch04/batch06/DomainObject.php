668735e9cb277979ef5e73f83b9986ead8aea5d3
Gene Kelley <phpcodehead@gmail.com>
2024-04-10 15:40:48 -0500

    Chapter 4, batch 6 & 6_1 complete.

---
+++ src/ch04/batch06/DomainObject.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace popp\ch04\batch06;
+
+/* listing 04.52, page 106 */
+abstract class DomainObject 
+{
+    public static function create(): DomainObject 
+    {
+        return new self();
+    }
+}
