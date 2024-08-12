8fb95f349da5e87c29a2d61ef2a52aea5133f646
Gene Kelley <phpcodehead@gmail.com>
2024-08-12 14:18:18 -0500

    Ch08Batch02::testLectureAndSeminar() passing with Lecture using FixedCostStrategy.

--- test/ch08/Ch08Batch02Test.php
+++ test/ch08/Ch08Batch02Test.php
@@ -3,8 +3,10 @@
 namespace popp\test\ch08;
 
 use popp\test\BaseUnit;
-use popp\ch08\batch02\Seminar;
 use popp\ch08\batch02\TimedCostStrategy;
+use popp\ch08\batch02\Seminar;
+use popp\ch08\batch02\Lecture;
+use popp\ch08\batch02\FixedCostStrategy;
 
 class Ch08Batch02Test extends BaseUnit
 {
@@ -15,6 +17,9 @@ class Ch08Batch02Test extends BaseUnit
         self::assertEquals(4, $seminar->duration());
         self::assertEquals(20, $seminar->cost());
 
-        //
+        $lecture = new Lecture(4, new FixedCostStrategy());
+        self::assertEquals('fixed rate', $lecture->chargeType());
+        self::assertEquals(4, $lecture->duration());
+        self::assertEquals(30, $lecture->cost());
     }
 }
