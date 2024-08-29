bb2a3107cd9f78a94033de244f34343c5ca86b91
Gene Kelley <phpcodehead@gmail.com>
2024-08-29 16:15:50 -0500

    Ch09Batch15Test::testNonArgInjection() passing.

--- test/ch09/Ch09Batch15Test.php
+++ test/ch09/Ch09Batch15Test.php
@@ -2,6 +2,7 @@
 
 namespace popp\test\ch09;
 
+use popp\ch09\batch15\ObjectAssembler;
 use popp\test\BaseUnit;
 use popp\ch09\batch15\Runner;
 use popp\ch09\batch11\Sea;
@@ -19,5 +20,19 @@ class Ch09Batch15Test extends BaseUnit {
     {
         $val = $this->capture(function() { Runner::run(); });
         self::assertEquals("Appointment data encoded in BloggsCal format\n", $val);
+    }
+
+    public function testNonArgInjection()
+    {
+        $assembler = new ObjectAssembler('src/ch09/batch15/objects.xml');
+
+        $sea = $assembler->getComponent(Sea::class);
+        self::assertTrue($sea instanceof EarthSea);
+
+        $plains = $assembler->getComponent(Plains::class);
+        self::assertTrue($plains instanceof MarsPlains);
+
+        $forest = $assembler->getComponent(Forest::class);
+        self::assertTrue($forest instanceof EarthForest);
     }
 }
