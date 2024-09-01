195867a8ec685974c1729a3f8ab6380c3fe7a116
Gene Kelley <phpcodehead@gmail.com>
2024-09-01 16:18:30 -0500

    Ch10Batch04Test::testArcher() passing.

--- test/ch10/Ch10Batch04Test.php
+++ test/ch10/Ch10Batch04Test.php
@@ -6,6 +6,7 @@ use popp\test\BaseUnit;
 use popp\ch10\batch04\Tank;
 use popp\ch10\batch04\Soldier;
 use popp\ch10\batch04\Army;
+use popp\ch10\batch04\Archer;
 
 class Ch10Batch04Test extends BaseUnit
 {
@@ -24,5 +25,18 @@ class Ch10Batch04Test extends BaseUnit
 
         $army->removeUnit($tank2);
         self::assertCount(2, $army->getUnits());
+    }
+
+    public function testArcher()
+    {
+        $archer = new Archer();
+        $tank = new Tank();
+
+        try {
+            $archer->addUnit($tank);
+            self::fail("Exception should have been thrown");
+        } catch (\Exception $e) {
+            self::assertEquals("popp\\ch10\\batch04\\Archer is a leaf", $e->getMessage());
+        }
     }
 }
