d014dd9a5a713aa63d6a3056d105e2f98c271a77
Gene Kelley <phpcodehead@gmail.com>
2024-09-30 15:10:39 -0500

    Chapter 11, Batch 03 complete.

---
+++ src/ch11/batch02/AVQuestion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace popp\ch11\batch02;
+
+use popp\ch11\batch02\Question;
+
+/* listing 11.16 */
+class AVQuestion extends Question
+{
+    // do audiovisual question specific things
+}
