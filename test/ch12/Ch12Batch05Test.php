9c494388aedef9fa1afc7439ff5ca086b6bdc729
Gene Kelley <phpcodehead@gmail.com>
2024-10-28 15:59:23 -0500

    Ch12Batch05Test::testRegistry() passing.

--- test/ch12/Ch12Batch05Test.php
+++ test/ch12/Ch12Batch05Test.php
@@ -5,6 +5,9 @@ namespace ch12;
 use popp\test\BaseUnit;
 use popp\ch12\batch05\Runner;
 use popp\ch12\batch05\AppException;
+use popp\ch12\batch05\TestRequest;
+use popp\ch12\batch05\Registry;
+use popp\ch12\batch05\Conf;
 
 class Ch12Batch05Test extends BaseUnit {
     public function testRunner()
@@ -15,5 +18,14 @@ class Ch12Batch05Test extends BaseUnit {
 
         $e = new AppException();
         self::assertInstanceOf(AppException::class, $e);
+    }
+
+    public function testRegistry() {
+        $request  = new TestRequest();
+        $registry = Registry::instance();
+        $registry->setRequest($request);
+
+        $conf = $registry->getConf();
+        self::assertInstanceOf(Conf::class, $conf);
     }
 }
