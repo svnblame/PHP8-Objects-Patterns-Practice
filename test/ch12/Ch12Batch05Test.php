f637ee5bfbe83f477f084c9925a9a411e8b38040
Gene Kelley <phpcodehead@gmail.com>
2024-11-03 15:40:03 -0600

    Chapter 12, Batch 5 complete.

--- test/ch12/Ch12Batch05Test.php
+++ test/ch12/Ch12Batch05Test.php
@@ -2,6 +2,7 @@
 
 namespace ch12;
 
+use popp\ch12\batch05\CommandResolver;
 use popp\test\BaseUnit;
 use popp\ch12\batch05\Runner;
 use popp\ch12\batch05\AppException;
@@ -9,6 +10,8 @@ use popp\ch12\batch05\TestRequest;
 use popp\ch12\batch05\Registry;
 use popp\ch12\batch05\Conf;
 use popp\ch12\batch05\ApplicationHelper;
+use popp\ch12\batch05\DefaultCommand;
+use popp\test\ch12\TestCommandBatch05;
 
 class Ch12Batch05Test extends BaseUnit {
     public function testRunner()
@@ -40,5 +43,53 @@ class Ch12Batch05Test extends BaseUnit {
         $dsn = $registry->getDSN();
 
         self::assertEquals("sqlite:/Users/gene/code/p8opp/src/ch12/batch05/data/woo.db", $dsn);
+    }
+
+    public function testCommandResolver()
+    {
+        // set up basics
+        $request = new TestRequest();
+        $registry = Registry::instance();
+        $registry->setRequest($request);
+
+        $commands = new Conf();
+        $resolver = new CommandResolver();
+
+        // no path set
+        $registry->setCommands($commands);
+        $command = $resolver->getCommand($request);
+        $feedback = $request->getFeedback();
+
+        self::assertEquals("path '/' not matched", $feedback[0]);
+        self::assertInstanceOf(DefaultCommand::class, $command);
+
+        // no-matching path
+        $request->clearFeedback();
+        $request->setPath('/testcmd');
+        $command = $resolver->getCommand($request);
+        $feedback = $request->getFeedback();
+
+        self::assertEquals("path '/testcmd' not matched", $feedback[0]);
+        self::assertInstanceOf(DefaultCommand::class, $command);
+
+        // matching path but no class
+        $request->clearFeedback();
+        $commands->set('/testcmd', '\\popp\\ch12\\batch05\\NoSuchClass');
+        $request->setPath('/testcmd');
+        $command = $resolver->getCommand($request);
+        $feedback = $request->getFeedback();
+
+        self::assertEquals("class '\\popp\\ch12\\batch05\\NoSuchClass' not found", $feedback[0]);
+        self::assertInstanceOf(DefaultCommand::class, $command);
+
+        // matching path and existing class
+        $request->clearFeedback();
+        $commands->set('/testcmd', '\\popp\\test\\ch12\\TestCommandBatch05');
+        $request->setPath('/testcmd');
+        $command = $resolver->getCommand($request);
+        $feedback = $request->getFeedback();
+
+        self::assertCount(0, $feedback);
+        self::assertInstanceOf(TestCommandBatch05::class, $command);
     }
 }
