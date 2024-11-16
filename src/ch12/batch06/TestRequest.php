65e7fa425c975d723b51842c08eb61e2a8ebaf3b
Gene Kelley <phpcodehead@gmail.com>
2024-11-16 01:56:30 -0600

    Ch12Batch06Test::testRegistry() passing.

---
+++ src/ch12/batch06/TestRequest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace popp\ch12\batch06;
+
+class TestRequest extends Request
+{
+    protected function init(): void
+    {
+        // TODO: Implement init() method.
+    }
+
+    public function forward(string $path): void
+    {
+        // TODO: Implement forward() method.
+    }
+}
--- test/ch12/Ch12Batch06Test.php
+++ test/ch12/Ch12Batch06Test.php
@@ -4,12 +4,35 @@ namespace ch12;
 
 use popp\test\BaseUnit;
 use popp\ch12\batch06\Runner;
+use popp\ch12\batch06\TestRequest;
+use popp\ch12\batch06\Registry;
+use popp\ch12\batch06\Conf;
+use popp\ch12\batch06\Request;
+
 
 class Ch12Batch06Test extends BaseUnit {
+    protected function setUp(): void
+    {
+        Registry::reset();
+    }
+
     public function testRunner()
     {
         $val = $this->capture(function() { Runner::run(); });
 
         self::assertMatchesRegularExpression('/Welcome to WOO/', $val);
+    }
+
+    public function testRegistry()
+    {
+        $request = new TestRequest();
+        $registry = Registry::instance();
+        $registry->setRequest($request);
+
+        $config = $registry->getConf();
+        self::assertTrue($config instanceof Conf);
+
+        $request = $registry->getRequest();
+        self::assertTrue($request instanceof Request);
     }
 }
