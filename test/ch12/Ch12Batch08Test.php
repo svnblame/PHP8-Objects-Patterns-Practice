f68c15ca87b9ec3910e0fceb0a4853991676b13b
Gene Kelley <phpcodehead@gmail.com>
2024-11-20 14:32:37 -0600

    Chapter 12, Batch 8 complete.

--- test/ch12/Ch12Batch08Test.php
+++ test/ch12/Ch12Batch08Test.php
@@ -4,6 +4,8 @@ namespace ch12;
 
 use popp\test\BaseUnit;
 use popp\ch12\batch08\Runner;
+use popp\ch12\batch08\PageController;
+use popp\ch12\batch06\Request;
 
 class Ch12Batch08Test extends BaseUnit {
     public function testRunner()
@@ -15,6 +17,22 @@ class Ch12Batch08Test extends BaseUnit {
         self::assertMatchesRegularExpression('/name is a required field/', $val);
 
         $val = $this->capture(function() { Runner::run3(); });
-        self::assertMatchesRegularExpression("/Error/", $val);
+        self::assertMatchesRegularExpression("/this is fallback/", $val);
+    }
+
+    public function testPageController()
+    {
+        $test = new class extends PageController {
+            public function process(): void {}
+        };
+        $controller = new $test();
+        $controller->init();
+        $request = $controller->getRequest();
+        self::assertInstanceOf(Request::class, $request);
+
+        ob_start();
+        $controller->forward(__DIR__.'/error.php');
+        $output = ob_get_clean();
+        self::assertEquals("this is fallback\n", $output);
     }
 }
