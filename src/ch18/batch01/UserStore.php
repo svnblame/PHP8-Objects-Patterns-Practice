b944744e9c9cd9bc6eee4f3ce5c357db1d3135d9
Gene Kelley <phpcodehead@gmail.com>
2025-05-07 15:22:19 -0500

    Chapter18 Runner::run() test passing.

---
+++ src/ch18/batch01/UserStore.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace popp\ch18\batch01;
+
+/* listing 18.01 */
+class UserStore
+{
+    private array $users = [];
+
+    /**
+     * @throws \Exception
+     */
+    public function addUser(string $name, string $mail, string $pass): bool
+    {
+        if (isset($this->users[$mail])) {
+            throw new \Exception("User {$mail} already exists");
+        }
+
+        if (strlen($pass) < 5) {
+            throw new \Exception("Password must be at least 5 characters");
+        }
+
+        $this->users[$mail] = [
+            'pass' => $pass,
+            'mail' => $mail,
+            'name' => $name
+        ];
+
+        return true;
+    }
+
+    public function notifyPasswordFailure(string $mail): void
+    {
+        if (isset($this->users[$mail])) {
+            $this->users[$mail]['failed'] = time();
+        }
+    }
+
+    public function getUser(string $mail): array
+    {
+        return ($this->users[$mail] ?? []);
+    }
+}
--- test/ch18/Ch18Batch01Test.php
+++ test/ch18/Ch18Batch01Test.php
@@ -2,14 +2,20 @@
 
 declare(strict_types=1);
 
-namespace popp\ch18;
+namespace ch18;
 
 use popp\BaseUnit;
+use popp\ch18\batch01\Runner;
 
 class Ch18Batch01Test extends BaseUnit
 {
     public function testRunner()
     {
         $val = $this->capture(function() { Runner::run(); });
+
+        self::assertMatchesRegularExpression("/\[pass] => 12345/", $val);
+        self::assertMatchesRegularExpression("/\[mail] => bob@example.com/", $val);
+        self::assertMatchesRegularExpression("/\[name] => bob williams/", $val);
+        self::assertMatchesRegularExpression("/\[failed] => \d+/", $val);
     }
 }
